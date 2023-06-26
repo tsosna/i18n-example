7690d025454795c61f705912bad6332760739d8d
Tomek Sosinski <tomek.sosinski@gmail.com>
2023-06-26 17:33:31 +0200

    Add Button components

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import {i, language, languages, switchLanguage} from '@inlang/sdk-js'
+	import {Button} from 'components/ui/button';
 </script>
 
 {#each languages as lang}
- <button on:click={()=>switchLanguage(lang)}>{lang}</button>  
+ <Button variant='secondary' class="mx-2 border border-gray-300 shadow-md" on:click={()=>switchLanguage(lang)}>{lang}</Button>
+ <!-- <button on:click={()=>switchLanguage(lang)}>{lang}</button>   -->
 {/each}
 
 
--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,17 @@
 // import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import preprocess from 'svelte-preprocess'
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import('@sveltejs/kit').Config}*/
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
 	preprocess: [
-			vitePreprocess({}),
-			preprocess ({preprocess:true})
-		],
-
+		vitePreprocess({}),
+		preprocess({
+			preprocess: true
+		})
+	],
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
@@ -21,7 +21,9 @@ const config = {
 			components: 'src/lib/components',
 			'$components/*': 'src/lib/components/*'
 		}
+	},
+	shadcn: {
+		componentPath: './src/lib/components/ui'
 	}
 };
-
 export default config;
