2f34394ce2b51f29a86d1676161eb13bef264027
Tomek Sosinski <tomek.sosinski@gmail.com>
2023-06-30 19:34:36 +0200

    Add  config.ts

---
+++ src/lib/config.ts
@@ -0,0 +1,6 @@
+import { dev } from "$app/environment"
+import { i } from "@inlang/sdk-js"
+
+export const title = 'nomadoM'
+export const description = i('description-web-site')
+export const url = dev ? 'http://localhost:5173/' : 'url'
