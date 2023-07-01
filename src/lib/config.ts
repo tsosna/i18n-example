30c46ad1c6a976c24735b6d8356027310c53b80c
Tomek Sosinski <tomek.sosinski@gmail.com>
2023-07-01 19:25:49 +0200

    Add Prisma and Supabase

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,7 @@
+import { PUBLIC_URL, PUBLIC_URL_DEV } from "$env/static/public"
 import { dev } from "$app/environment"
 import { i } from "@inlang/sdk-js"
 
 export const title = 'nomadoM'
 export const description = i('description-web-site')
-export const url = dev ? 'http://localhost:5173/' : 'url'
+export const url = dev ? PUBLIC_URL_DEV : PUBLIC_URL
